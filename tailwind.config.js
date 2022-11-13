7918da41b299f08489ed218b0483c3086358d1e4
spokhrel76652 <pokhrelgopal27@gmail.com>
2022-11-13 14:33:51 +0545

    hello

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    fontFamily: {
+      'sans': ['Poppins', 'sans-serif'],
+    },
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        blueGray: '#D9E4EC',
+        mistyBlue: '#B7CFDC',
+        lightBlue: '#6AABD2',
+        darkBlue: '#385E72',
+        bluest: '#4A6CF7',
+        grayish: '#1D232D',
+        golden: '#ffdf01',
+      },
+    },
+  },
+  plugins: [],
+}
